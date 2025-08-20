b7570808ceaf8c6914bf3e492be5c64c86431718
Jacob Foshee <jacobf@gmail.com>
2025-08-20 17:00:31 -0500

    Use conventional imports

--- extension/test/unit/testEnvFiles.test.ts
+++ extension/test/unit/testEnvFiles.test.ts
@@ -4,9 +4,9 @@
  *--------------------------------------------------------*/
 
 import assert from 'assert';
-import * as path from 'path';
-import * as fs from 'fs';
-import * as os from 'os';
+import path from 'path';
+import fs from 'fs';
+import os from 'os';
 import { parseEnvFiles } from '../../src/utils/envUtils';
 
 suite('parseEnvFiles Tests', () => {
